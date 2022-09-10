a2c34e97d1e90463943a72b37a3f0af18c159bfd
Aleeh <alexandreadabo_@hotmail.com>
2022-09-10 10:33:14 -0300

    Project created

---
+++ backend/src/test/java/com/AlexandreDev/dsmeta/DsmetaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AlexandreDev.dsmeta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsmetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
