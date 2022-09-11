1f2e01059429429a3309665d0c55b69527256029
Aleeh <alexandreadabo_@hotmail.com>
2022-09-11 20:42:32 -0300

    Api test

---
+++ backend/src/main/java/com/AlexandreDev/dsmeta/repositories/SaleRepository.java
@@ -0,0 +1,9 @@
+package com.AlexandreDev.dsmeta.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.AlexandreDev.dsmeta.entities.Sale;
+
+public interface SaleRepository extends JpaRepository<Sale, Long > {
+
+}
---
+++ backend/src/main/java/com/AlexandreDev/dsmeta/services/SaleService.java
@@ -0,0 +1,20 @@
+package com.AlexandreDev.dsmeta.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.AlexandreDev.dsmeta.entities.Sale;
+import com.AlexandreDev.dsmeta.repositories.SaleRepository;
+
+@Service
+public class SaleService {
+	
+	@Autowired
+	private SaleRepository repository;
+	
+	public List<Sale> findSales(){
+		return repository.findAll();
+	}
+}
